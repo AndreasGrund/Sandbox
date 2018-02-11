2a902cf458505712db4523ac29dc7b964999560b
andi grund <andi_grund@web.de>
2018-02-11 22:37:12 +0100

    function for train test split + logging

---
+++ src/data/get_data.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+import os
+import json
+import logging
+import tarfile
+from six.moves import urllib
+import pandas as pd
+
+# logging
+current_dir = os.path.dirname(__file__)
+logger = logging.getLogger('get_data')
+logging.basicConfig(filename=os.path.join(os.path.join(os.path.abspath(os.path.join(current_dir, "../..")), 'logs'), 'get_data'),level=logging.DEBUG)
+
+
+class GetData:
+
+    # load config
+    config_dir = os.path.join(os.path.abspath(os.path.join(current_dir, "../..")), 'config')
+    data_config = os.path.join(config_dir, 'data_import.json')
+    with open(data_config) as data_file:
+        data = json.load(data_file)
+
+    # set up parameter
+    _RAW = data['store_raw_data']
+    _PATH = os.path.join(os.path.abspath(os.path.join(current_dir, "../..")), _RAW)
+    _Project = data['project_name']
+    _Project_Path = os.path.join(_PATH, _Project)
+    _FILE = data['csv_file']
+    _file = data['raw_file']
+    _URL = data['url']
+
+
+    def fetch_data(self, url=_URL, path=_Project_Path, file=_file):
+        """
+
+        """
+        logger.info('fetch data from web: {}'.format(url))
+        print('fetch data from web: {}'.format(url))
+
+        if not os.path.isdir(path):
+            os.makedirs(path)
+        tgz_path = os.path.join(path, file)
+        urllib.request.urlretrieve(url, tgz_path)
+        file_tgz = tarfile.open(tgz_path)
+        file_tgz.extractall(path=path)
+        file_tgz.close()
+
+
+    def load_local_csv(self, path=_Project_Path, file=_FILE):
+        """
+
+        """
+        logger.info('load local data from {}'.format(path))
+
+        self.fetch_data()
+
+        print('load local data from {}'.format(path))
+        csv_path = os.path.join(path, file)
+        return pd.read_csv(csv_path)
