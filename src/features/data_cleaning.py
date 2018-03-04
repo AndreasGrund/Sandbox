21027b276589a327659a82df194ef1c022cd89c5
andi grund <andi_grund@web.de>
2018-03-04 17:04:48 +0100

    small refactoring

--- src/features/data_cleaning.py
+++ src/features/data_cleaning.py
@@ -58,6 +58,7 @@ class LabelBinarizerPipelineFriendly(LabelBinarizer):
     def fit(self, X, y=None):
         """this would allow us to fit the model based on the X input."""
         super(LabelBinarizerPipelineFriendly, self).fit(X)
+
     def transform(self, X, y=None):
         return super(LabelBinarizerPipelineFriendly, self).transform(X)
 
