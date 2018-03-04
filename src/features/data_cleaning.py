975fdf63729aea875777d954e5b7b725bc9718bd
andi grund <andi_grund@web.de>
2018-03-04 16:48:02 +0100

    data prep pipeline

--- src/features/data_cleaning.py
+++ src/features/data_cleaning.py
@@ -8,6 +8,10 @@ import numpy as np
 
 from sklearn.preprocessing import Imputer
 from sklearn.preprocessing import LabelBinarizer
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import FeatureUnion
 
 from config.config import config
 from features.train_test_split import TrainTestSplit
@@ -17,6 +21,48 @@ current_dir = os.path.dirname(__file__)
 logger = logging.getLogger('pipeline')
 logging.basicConfig(filename=os.path.join(os.path.join(os.path.abspath(os.path.join(current_dir, "../..")), 'logs'),
                                           'pipeline'), level=logging.DEBUG)
+
+
+class DataFrameSelector(BaseEstimator, TransformerMixin):
+    def __init__(self, attribute_names):
+        self.attribute_names = attribute_names
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        return X[self.attribute_names].values
+
+
+rooms_ix, bedrooms_ix, population_ix, household_ix = 3, 4, 5, 6
+
+
+class CombinedAttributesAdder(BaseEstimator, TransformerMixin):
+    def __init__(self, add_bedrooms_per_room=True):
+        self.add_bedrooms_per_room = add_bedrooms_per_room
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X, y=None):
+        rooms_per_household = X[:, rooms_ix] / X[:, household_ix]
+        population_per_household = X[:, population_ix] / X[:, household_ix]
+        if self.add_bedrooms_per_room:
+            bedrooms_per_room = X[:, bedrooms_ix] / X[:, rooms_ix]
+            return np.c_[X, rooms_per_household, population_per_household, bedrooms_per_room]
+        else:
+            return np.c_[X, rooms_per_household, population_per_household]
+
+
+class LabelBinarizerPipelineFriendly(LabelBinarizer):
+    def fit(self, X, y=None):
+        """this would allow us to fit the model based on the X input."""
+        super(LabelBinarizerPipelineFriendly, self).fit(X)
+    def transform(self, X, y=None):
+        return super(LabelBinarizerPipelineFriendly, self).transform(X)
+
+    def fit_transform(self, X, y=None):
+        return super(LabelBinarizerPipelineFriendly, self).fit(X).transform(X)
 
 
 class DataCleaning(luigi.Task):
@@ -44,33 +90,49 @@ class DataCleaning(luigi.Task):
 
         def num_inputer():
 
-            inputer = Imputer(strategy=self.input_strategy)
-
             train_num = train.select_dtypes(include=[np.number])
+            num_attribs = list(train_num)
 
-            inputer.fit(train_num)
+            num_steps = Pipeline([
+                ('selector', DataFrameSelector(num_attribs)),
+                ('imputer', Imputer(strategy=self.input_strategy)),
+                ('attribs_adder', CombinedAttributesAdder()),
+                ('std_scaler', StandardScaler()),
+            ])
 
-            X = inputer.transform(train_num)
-
-            train_num = pd.DataFrame(X, columns=train_num.columns)
-
-            return train_num
+            return num_steps
 
         def label_encoder():
 
-            encoder = LabelBinarizer()
-
             train_cat = train.select_dtypes(include=[np.chararray])
 
-            train_cat_encoded = encoder.fit_transform(train_cat)
+            cat_attribs = list(train_cat)
 
-            return train_cat_encoded
+            cat_steps = Pipeline([
+                ('selector', DataFrameSelector(cat_attribs)),
+                ('label_binarizer', LabelBinarizerPipelineFriendly()),
+            ])
 
-        test = label_encoder()
+            return cat_steps
 
+        def full_pipeline():
 
+            full_pipeline = FeatureUnion(transformer_list=[
+                ('num_pipeline', num_pipeline),
+                ('cat_pipeline', cat_pipeline),
+            ])
 
+            return full_pipeline
 
+        num_pipeline = num_inputer()
+
+        cat_pipeline = label_encoder()
+
+        full_pipeline = full_pipeline()
+
+        data_prepared = full_pipeline.fit_transform(train)
+
+        data_prepared.shape
 
 
 if __name__ == "__main__":
